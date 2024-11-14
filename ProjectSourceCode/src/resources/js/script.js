8995ff4beb263f87ec17af9392511cc25e66c669
Alex Recana <alexandria.recana@colorado.edu>
2024-11-13 18:08:47 -0700

    added sound button to control muting in settings and implemented in script

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -20,3 +20,17 @@ function setDifficulty(level){
         
     }
 }
+
+function toggleMute() {
+    isMuted = !isMuted;
+    // audioElements is just a temp variable for now 
+    const audioElements = document.querySelectorAll("audio");
+
+    // loop to mute all the aduios we're going to set up
+    audioElements.forEach(audio => {
+      audio.muted = isMuted;
+    });
+
+    // this updates the toggle button to say unmute/mute depending on what its on right now
+    document.getElementById("muteButton").innerText = isMuted ? "Unmute" : "Mute";
+}
