3140c43aa22a3a2234f0e6abf5dab03b112ce9d6
Coleisgay45 <63007624+Coleisgay45@users.noreply.github.com>
2024-11-14 10:51:51 -0800

    home page

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -77,11 +77,10 @@ app.get('/', (req, res) => {
   res.render('pages/register');
 });
 
-
-
 app.get('/login', (req, res) => {
     res.render('pages/login');
 });
+
 app.get('/', (req, res) => {
   res.render('pages/home', { 
   });
