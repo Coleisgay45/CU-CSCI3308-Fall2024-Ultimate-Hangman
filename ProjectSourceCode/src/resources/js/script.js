55be11121dfceacc1161162f395b166093bebd9e
Dorrin Ashrafi <dorrin.ashrafi22@gmail.com>
2024-11-17 13:24:39 -0700

    changes

--- ProjectSourceCode/src/resources/js/script.js
+++ ProjectSourceCode/src/resources/js/script.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+//library for reading files
 function setTheme(theme) {
     const body = document.body;
     if (theme === 'Light') {
@@ -9,14 +11,44 @@ function setTheme(theme) {
 
 function setDifficulty(level){
     if (level == 'Easy'){
-
+      // 3 and 4 
     }
 
     else if (level == 'Medium'){
-
+   // 5 6 
     }
 
     else if (level == 'Hard'){
-        
+           // 6+
     }
 }
+
+function WordsFromFile(){
+  fs.readFile('wordsanddefinitions.txt','utf8',(err,data)=>{
+    if (err){
+      console.log("Error in loading file");
+      return;
+    }
+    else{
+      console.log('ok');
+    }
+    const lines = data.split('\n');
+    const words = [];
+    const definitions =[];
+    lines.forEach(line =>{
+      const [word,definition] = line.split(',').map(part=>part.trim());
+   
+      if(word && definition){
+        words.push(word);
+        definitions.push(definition);
+      }
+    });
+
+    console.log(words);
+    console.log(definitions);
+
+
+  });
+}
+
+WordsFromFile();
