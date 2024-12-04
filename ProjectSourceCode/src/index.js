9e0f591ab991486c365bac7bfbb54633f6492a67
Erasmia <erpoulas@gmail.com>
2024-12-04 09:45:01 -0700

    get rid of extra get home endpoint

--- ProjectSourceCode/src/index.js
+++ ProjectSourceCode/src/index.js
@@ -247,10 +247,6 @@ app.get('/leaderboard', (req, res) => {
     res.render('pages/leaderboard');
 });
 
-app.get('/home', (req,res) => {
-    res.render('pages/home')
-});
-
 // access after this point requires login 
 // TODO: do we write test case for this ?
 const auth = (req, res, next) => {
