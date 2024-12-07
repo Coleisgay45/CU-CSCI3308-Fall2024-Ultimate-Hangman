88512cf95ded4fb4de26cfee7e9186d6e4c7604e
Coleisgay45 <63007624+Coleisgay45@users.noreply.github.com>
2024-12-06 19:07:33 -0700

    wa

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -7,7 +7,7 @@
 PG_URI="postgresql://users_db_8r8u_user:YJPBqxCj6R8xmolKyIIjuerMpt0UGCYp@dpg-csvoebt6l47c73dp7v2g-a.oregon-postgres.render.com/users_db_8r8u"
 #
 # Execute each .sql file in the directory
-for file in init_data/*.sql; do
+for file in src/init_data/*.sql; do
     echo "Executing $file..."
     psql $PG_URI -f "$file"
 done
